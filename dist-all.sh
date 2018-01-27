9fdaca0f0ffee766cb4ecec61cfd87ff967ba1e6
Dan McNulty <mcnulty@users.noreply.github.com>
2018-01-27 14:20:40 -0600

    Experimenting with travis stages #1

---
+++ dist-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# A script to package all platform artifacts into a single package
+#
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 version"
+    exit 1
+fi
+
+VERSION=$1
+BASE_URL="https://github.com/udidb/native-file-tests/releases/download/$VERSION/native-file-tests"
+
+URLS=($BASE_URL-macos-$VERSION.zip $BASE_URL-linux-$VERSION.zip)
+
+for url in ${URLS[@]}; do
+    curl -sSfL $url || {
+        echo "Failed to download $url"
+        exit 1
+    }
+
+    file_name=$(basename $url)
+
+    unzip -j $file_name -d native-file-tests || {
+        echo "Failed to extract $file_name"
+        exit 1
+    }
+done
