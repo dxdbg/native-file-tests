3f2a34148ec9eed7cff4c16bbf3b6de0d3df9123
Dan McNulty <mcnulty@users.noreply.github.com>
2018-01-27 13:54:57 -0600

    First step for packaging release with travis

--- dist.sh
+++ dist.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 #
-# Script to populate the dist directory from a build
+# Script to package a .zip containing the artifacts
 #
 
-rm -rf dist
-mkdir dist
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <platform name> <version>"
+    exit 1
+fi
+
+PLATFORM_NAME=$1
+VERSION=$2
+
+mkdir native-file-tests-$PLATFORM_NAME-$VERSION
 
 for file in `ls ./build/*/*`; do
     base=`basename $file`
     sha1=`sha1sum $file | awk '{ print $1 }'`
-    cp $file dist/$base.$sha1
+    cp $file native-file-tests-$PLATFORM_NAME-$VERSION/$base.$sha1
 done
+
+zip -r native-file-tests-$PLATFORM_NAME-$VERSION.zip native-file-tests-$PLATFORM_NAME-$VERSION/
